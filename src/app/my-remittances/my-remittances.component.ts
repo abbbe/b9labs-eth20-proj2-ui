7ad75b1af492cb88ecbe4f32ee32dbd1c723b673
Alexandre Bezroutchko <abb@gremwell.com>
2018-02-04 22:58:01 +0100

    implement list of remittances

--- src/app/my-remittances/my-remittances.component.ts
+++ src/app/my-remittances/my-remittances.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Remittance } from '../remittance';
 
 @Component({
   selector: 'app-my-remittances',
@@ -6,10 +7,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./my-remittances.component.css']
 })
 export class MyRemittancesComponent implements OnInit {
+  remittances: Remittance[] = new Array<Remittance>()
 
   constructor() { }
 
   ngOnInit() {
+    // FIXME pupulate from events
+    this.remittances.push(new Remittance('x-sender', 'x-recipient', 1, 'x-otphash'));
   }
 
+  onRevokeClicked(otpHash) {
+    console.log("onRevokeClicked: optHash:", otpHash);
+    // FIXME call smartcontract
+  }
 }
