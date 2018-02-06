621e7644f5cd1b8366a035b8b7c87cad6ca563c3
Alexandre Bezroutchko <abb@gremwell.com>
2018-02-06 02:10:11 +0100

    remove my-claims

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,6 @@ import { AppComponent } from './app.component';
 import { NewRemittanceComponent } from './new-remittance/new-remittance.component';
 import { MyRemittancesComponent } from './my-remittances/my-remittances.component';
 import { NewClaimComponent } from './new-claim/new-claim.component';
-import { MyClaimsComponent } from './my-claims/my-claims.component';
 import { EthDashboardComponent } from './eth-dashboard/eth-dashboard.component';
 import { RemittanceService } from './remittance.service';
 import { Web3Service } from './web3.service';
@@ -19,7 +18,6 @@ import { Web3Service } from './web3.service';
     NewRemittanceComponent,
     MyRemittancesComponent,
     NewClaimComponent,
-    MyClaimsComponent,
     EthDashboardComponent
   ],
   imports: [
--- src/app/claim.ts
+++
@@ -1,7 +0,0 @@
-export class Claim {
-    constructor(
-        public sender: string,
-        public recipient: string,
-        public amount: number,
-        public otpHash: string) { }
-}
